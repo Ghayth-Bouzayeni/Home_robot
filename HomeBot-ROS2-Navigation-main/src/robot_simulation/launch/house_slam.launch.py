e39e5b8bbd39d7c3abde8a2162515407e5b66ace
Ghayth Bouzayani <ghayth.bouzayani@ensi-uma.tn>
2025-02-17 22:07:06 +0100

    house_slam_some_modf_1

--- HomeBot-ROS2-Navigation-main/src/robot_simulation/launch/house_slam.launch.py
+++ HomeBot-ROS2-Navigation-main/src/robot_simulation/launch/house_slam.launch.py
@@ -11,14 +11,20 @@ def generate_launch_description():
     # Define package paths
     pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')
     pkg_robot_description = FindPackageShare(package='robot_description').find('robot_description')
+    pkg_robot_simulation = FindPackageShare(package='robot_simulation').find('robot_simulation')
 
     # Define file paths
     default_model_path = os.path.join(pkg_robot_description, 'urdf/mobile_robot.urdf.xacro')
+    robot_localization_file_path = os.path.join(pkg_robot_simulation, 'config/ekf.yaml')
+    default_rviz_config_path = os.path.join(pkg_robot_simulation, 'config/nav2_config.rviz')
+    world_file_name = 'house.world'
+    world_path = os.path.join(pkg_robot_simulation, 'worlds', world_file_name)
 
     # Launch configuration variables
     model = LaunchConfiguration('model')
     rviz_config_file = LaunchConfiguration('rviz_config_file')
     use_sim_time = LaunchConfiguration('use_sim_time')
+    use_simulator = LaunchConfiguration('use_simulator')
     
     # Declare launch arguments
     declare_model_path_cmd = DeclareLaunchArgument(
@@ -29,7 +35,7 @@ def generate_launch_description():
     
     declare_rviz_config_file_cmd = DeclareLaunchArgument(
         name='rviz_config_file',
-        default_value=os.path.join(pkg_gazebo_ros, 'config/rviz_config.rviz'),
+        default_value=default_rviz_config_path,
         description='Full path to RVIZ config file'
     )
     
@@ -39,10 +45,16 @@ def generate_launch_description():
         description='Use simulation (Gazebo) clock if true'
     )
 
+    declare_world_cmd = DeclareLaunchArgument(
+        name='world',
+        default_value=world_path,
+        description='Full path to the world model file to load'
+    )
+
     # Start Gazebo server
     start_gazebo_server_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
-        launch_arguments={'world': 'empty.world'}.items()  # Using a simple empty world
+        launch_arguments={'world': world_path}.items()
     )
 
     # Start Gazebo client    
@@ -56,6 +68,25 @@ def generate_launch_description():
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=['-topic', 'robot_description', '-entity', 'my_robot']
+    )
+
+    # Start robot localization using an Extended Kalman filter
+    start_robot_localization_cmd = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=[robot_localization_file_path, 
+        {'use_sim_time': use_sim_time}]
+    )
+
+    # Start SLAM
+    start_slam_toolbox_cmd = Node(
+        package='slam_toolbox',
+        executable='sync_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}]
     )
 
     # Start robot state publisher
@@ -83,12 +114,15 @@ def generate_launch_description():
     ld.add_action(declare_model_path_cmd)
     ld.add_action(declare_rviz_config_file_cmd)
     ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_world_cmd)
 
     # Add actions to launch description
     ld.add_action(start_gazebo_server_cmd)
     ld.add_action(start_gazebo_client_cmd)
     ld.add_action(spawn_robot_cmd)
+    ld.add_action(start_robot_localization_cmd)
     ld.add_action(start_robot_state_publisher_cmd)
+    ld.add_action(start_slam_toolbox_cmd)
     ld.add_action(start_rviz_cmd)
 
     return ld
